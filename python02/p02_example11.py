e8cce193e59c0a06a9951d43f70c1df6a719be89
Łukasz Pawlik <lukasz.pawlik@altar.com.pl>
2024-03-15 09:21:51 +0100

    Max number

---
+++ python02/p02_example11.py
@@ -0,0 +1,27 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+# if condition
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("Max number:", largest)
+
+ # for condition
+numbers = []
+
+for i in range(1, 4):
+    num = int(input(f"Enter number {i}: "))
+    numbers.append(num)
+
+largest = numbers[0]
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Max number:", largest)
