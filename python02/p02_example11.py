201b83a1f6be722785abf4db03396ff7d8804716
Łukasz Pawlik <lukasz.pawlik@altar.com.pl>
2024-03-15 09:51:03 +0100

    Fix

--- python02/p02_example11.py
+++ python02/p02_example11.py
@@ -4,11 +4,11 @@ num3 = int(input("Enter the third number: "))
 
 # if condition
 if (num1 >= num2) and (num1 >= num3):
-   largest = num1
+    largest = num1
 elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
+    largest = num2
 else:
-   largest = num3
+    largest = num3
 
 print("Max number:", largest)
 
