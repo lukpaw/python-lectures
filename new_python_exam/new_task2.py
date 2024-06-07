d8ce9813938b384eb88caeecec2f10ede286b09a
lukpaw <lukpaw@gmail.com>
2024-06-07 08:18:25 +0200

    fix

---
+++ new_python_exam/new_task2.py
@@ -0,0 +1,14 @@
+"""
+==========================
+Task instructions:
+==========================
+This program changes text to lowercase and removes first names (from a predefined list) from an text.
+Predefined list of first names:
+"John", "Jane", "Michael", "Sarah", "David", "Jennifer", "Robert", "Elizabeth", "William", "Mary"
+
+==========================
+Example Output:
+==========================
+Enter the text: This is a sample text with names like John, Jane, and Michael. How about Sarah appearing later?
+Modified Text: this is a sample text with names like , , and . how about  appearing later?
+"""
